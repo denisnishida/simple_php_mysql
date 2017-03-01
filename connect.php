31aea20d445f41b82365723a0fbcfcd96cd62764
Denis T. Nishida <denis.nishida@gmail.com>
2017-03-01 13:30:17 -0600

    Heroku connections

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-    define('DB_DSN','mysql:host=localhost;dbname=serverside;charset=utf8');
-    define('DB_USER','serveruser');
-    define('DB_PASS','gorgonzola7!');
+    define('DB_DSN','pgsql:host=ec2-23-21-238-246.compute-1.amazonaws.com;port=5432;dbname=dbbqfb77qi7mpv');
+    define('DB_USER','mopareauqspvoi');
+    define('DB_PASS','1f326f36dbce42c9668300aac1d5927d9669d1ae60084d6a38bfad5968b4c083');
 
     // Create a PDO object called $db.
     $db = new PDO(DB_DSN, DB_USER, DB_PASS);
